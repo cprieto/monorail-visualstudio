0c214ac8f4822805f61e6b3777d7ca01019d4bb0
Cristian Prieto <cristian@rioshu.com>
2010-03-23 15:06:09 -0600

    Added routes to vb.net project, fixed typo in web.config in both project templates

---
+++ src/template-vb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="MonoRail.Template.Global_asax" Language="vb" %>
---
+++ src/template-vb/Global.asax.vb
@@ -0,0 +1,25 @@
+﻿Imports Castle.MonoRail.Framework.Routing
+
+Public Class Global_asax
+    Inherits HttpApplication
+
+    Sub Application_Start (ByVal sender As Object, ByVal e As EventArgs)
+        RegisterRules (RoutingModuleEx.Engine)
+    End Sub
+
+    Private Shared Sub RegisterRules (ByVal rules As IRoutingRuleContainer)
+        Dim rootRoute As New PatternRoute ("root", "/")
+        With rootRoute
+            .DefaultForController().Is ("Home")
+            .DefaultForAction().Is ("Index")
+        End With
+        rules.Add (rootRoute)
+
+        Dim defaultRoute As New PatternRoute ("standard", "[controller]/[action]/[id]")
+        With defaultRoute
+            .DefaultForController().Is ("Home")
+            .DefaultForAction().Is ("Index")
+        End With
+        rules.Add (defaultRoute)
+    End Sub
+End Class
