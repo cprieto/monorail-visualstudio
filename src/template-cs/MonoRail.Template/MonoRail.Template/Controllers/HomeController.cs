8fa76a24bc1ac30a90e0646a0766d585a684746a
cprieto <cristian@rioshu.com>
2010-03-22 23:06:16 -0600

    Added monorail basic configuration in web.config and basic routing rules

---
+++ src/template-cs/MonoRail.Template/MonoRail.Template/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Castle.MonoRail.Framework;
+
+namespace MonoRail.Template.Controllers
+{
+    public class HomeController : SmartDispatcherController
+    {
+        public void Index()
+        {
+        }
+    }
+}
